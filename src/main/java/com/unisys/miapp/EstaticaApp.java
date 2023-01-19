0dddc45a33d2d36903d930c89a4470222ad49b20
AlvarezM <tuky1026@gmail.com>
2023-01-19 17:01:34 +0100

    Ejercicio Clase Estática y Método modif

--- src/main/java/com/unisys/miapp/EstaticaApp.java
+++ src/main/java/com/unisys/miapp/EstaticaApp.java
@@ -2,7 +2,7 @@ package com.unisys.miapp;
 import static com.unisys.miapp.Estatica.berrinche;
 public class EstaticaApp {
     public static void main(String[] args) {
-        int pi = 0;
+        int pi = 2;
         System.out.println(pi);
         var quejido = berrinche();
         System.out.println(quejido);
