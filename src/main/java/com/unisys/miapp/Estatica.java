2b65de485687ec1e659ef9197fe9f888352d49c5
AlvarezM <tuky1026@gmail.com>
2023-01-19 16:52:13 +0100

    Ejercicio Clase Estática y Método

---
+++ src/main/java/com/unisys/miapp/Estatica.java
@@ -0,0 +1,15 @@
+package com.unisys.miapp;
+
+public class Estatica {
+
+    // atributo estático es compartido entre todas las instancias
+    public static int pi;
+
+    // Método estático 
+
+    public static String berrinche(){
+        return "ayyyy!!";
+    }
+
+    }
+
---
+++ src/main/java/com/unisys/miapp/EstaticaApp.java
@@ -0,0 +1,10 @@
+package com.unisys.miapp;
+import static com.unisys.miapp.Estatica.berrinche;
+public class EstaticaApp {
+    public static void main(String[] args) {
+        int pi = 0;
+        System.out.println(pi);
+        var quejido = berrinche();
+        System.out.println(quejido);
+    }
+}
